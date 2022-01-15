19154c869442e35438b71cbad82a35db8fc35c6c
Oleg Roganin <94141484+olegroganin@users.noreply.github.com>
2022-01-15 17:29:50 +0200

    Update ToDo.js

--- source/src/components/ToDo.js
+++ source/src/components/ToDo.js
@@ -3,16 +3,16 @@ import closeicon from '../closeicon.png';
 
 
 function ToDo ({todo, removeTask}) {
-	return (
-		<div>
-		   <div key={todo.id} className='todo-item'>
+   return (
+      <div>
+	 <div key={todo.id} className='todo-item'>
             {todo.task}
-	      <div className='todo-item-remove' onClick={() => removeTask(todo.id)}>
+	  <div className='todo-item-remove' onClick={() => removeTask(todo.id)}>
             <img src={closeicon} alt='icon'/>
          </div>
-			</div>
-		</div>
-	)
+	 </div>
+      </div>
+   )
 }
 
 export default ToDo
